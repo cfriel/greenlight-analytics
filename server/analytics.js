eac7793c9bd884f7e3ff0790f77764444de814dd
Chris Friel <cfriel@gmail.com>
2013-08-02 16:06:54 -0400

    refactoring package api

--- server/analytics.js
+++ server/analytics.js
@@ -4,12 +4,7 @@ var version = "1.0";
 Meteor.startup(function(){
     
     console.log("loading analytics package");
-    
-    if(!SiteTemplates.findOne( { name: name, version: version }))
-    {
-	console.log("registering " + name + " site template");
-	SiteTemplates.insert( { name : name, version : version } );
-	Greenlight.register_template(name, version);
-    }
-    
+
+    Greenlight.register_template(name, version);
+        
 });
